f3f63e5fdd3b54396eb0388b99109132f241ea53
Oriol Fernandez <oriol.developer@gmail.com>
2025-05-04 11:37:29 +0100

    Add a color picker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Jetpack Compose Color Picker
+    implementation(libs.compose.color.picker)
 }
